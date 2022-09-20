de57b4f0043503824efe4fb70c03062a7556166e
villep <pekvil16@gmail.com>
2022-09-20 15:01:52 +0300

    tehtava 1.6

--- src/App.js
+++ src/App.js
@@ -6,10 +6,42 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () =>{
+    setGood(good + 1)
+    console.log('good',good)
+  }
+  const handleNeutralClick = () =>{
+    setNeutral(neutral + 1)
+    console.log('neutral',neutral)
+  }
+  const handleBadClick = () =>{
+    setBad(bad + 1)
+    console.log('bad', bad)
+  }
+  
+
   return (
     <div>
-      code here
+      <Header/>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <Statistics/>
+      <p>Good : {good}</p>
+      <p>Neutral : {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
+  )
+}
+
+const Header = () =>{
+    return(
+      <h2>Give feeback</h2>
+    )
+}
+const Statistics = () =>{
+  return(
+    <h2>Statistics</h2>
   )
 }
 
